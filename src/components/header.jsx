fa7ef415579d1d8fb6741e631097713594e5f5d9
jasjeet774 <jasjeetkaur774@gmail.com>
2025-06-19 14:11:50 +0530

    setup Nextjs project,clean default settings,Create folders and components

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-gradient-to-r from-indigo-500 to-purple-600 p-6 shadow-md">
+      <h1 className="text-3xl font-extrabold text-white text-center tracking-wide">
+        📝 Task Manager
+      </h1>
+    </header>
+  );
+}
